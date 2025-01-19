218edba2d758f16dfcf95219ecdfd9dd6cf0816b
DasEntlein <nico.ernecker3@gmail.com>
2025-01-19 04:16:32 +0100

    the script

---
+++ client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while true do
+    SetFollowPedCamViewMode(4)
+    DisableControlAction(0, 0, true)
+    Wait(0)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'   -- Die FiveM-Version (aktuell 'cerulean' ist die neueste stabile Version)
+game 'gta5'            -- Das Spiel, für das dieses Skript ist (GTA 5)
+
+author 'DeinName'      -- Dein Name oder dein Pseudonym
+description 'Skript, das den Spieler automatisch in den First-Person-Modus setzt'
+version '1.0.0'        -- Versionsnummer des Skripts
+
+client_script 'client.lua' -- Verweis auf dein Haupt-Client-Skript
